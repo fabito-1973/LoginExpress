e9bc3714240e96dba7170380f6c308785fa7ccb5
fabito-1973 <ruizgordillofabiola@gmail.com>
2024-06-13 18:45:28 -0500

    modulo del proyecto

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 3000
 // Get the client
-const mysql = require('mysql2/promise');
+
 const cors = require('cors')
 const session = require('express-session')
+const md5 = require('md5');
+
+const login = require('./login');
+const registro = require('./registro');
+const { obtenerUsuarios, eliminarUsuario } = require('./usuarios');
+const validar = require('./validar');
+
 
 
 app.use(cors({
@@ -12,45 +19,22 @@ app.use(cors({
     credentials: true,
 }))
 app.use(session({
-    secret: "djudhhfttcbbcb"
+    secret: "djudhhfttcbbcb",
+
 }))
-// Create the connection to database
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'login',
-});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/login', async (req, res) => { // req: request, peticion; res: response, respuesta;
-    const datos = req.query;
-    // A simple SELECT query
-    try {
-        const [results, fields] = await connection.query(
-            "SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
-            [datos.usuario, datos.clave]
-        );
-        if (results.length > 0) {
-            req.session.usuario = datos.usuario;
-            res.status(200).send('Inicio de sesion correcto')
-        } else {
-            res.status(401).send('Inicio de sesion incorrecto')
-        }
-    } catch (error) {
-        console.log(error);
+app.get('/login', login);
+app.get('/validar', validar)
+app.get('/registro', registro);
 
-    }
-});
-app.get('/validar', (req, res) => {
-    if (req.session.usuario) {
-        res.status(200).send('Sesion validada')
-    } else {
-        res.status(401).send('No autorizado')
-    }
-})
+app.get('/usuarios', obtenerUsuarios);
+
+app.delete('/usuarios', eliminarUsuario);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
